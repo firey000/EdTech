a3607b977fce4432701f107439ce31d2570a5cc8
Kirill9863 <wwwkirill.gonch@gmail.com>
2026-04-28 14:30:23 +0300

    qwe

--- footer.php
+++ footer.php
@@ -1,21 +1,34 @@
-<footer class="site_footer">
-  <div class="footer_left">
-    <div class="logo">EdTech</div>
-    <div class="social_icons">
-      <div class="icon_box"></div>
-      <div class="icon_box"></div>
-      <div class="icon_box"></div>
-      <div class="icon_box"></div>
-    </div>
-  </div>
 
-  <div class="footer_nav">
-    <a href="#" class="nav_btn">Каталог курсов</a>
-    <a href="#" class="nav_btn">Учиться бесплатно</a>
-    <a href="#" class="nav_btn">Медиа Нетологии</a>
-  </div>
 
-  <div class="footer_right">
-    <div class="placeholder_square"></div>
-  </div>
-</footer>
+    <footer class="footer">
+        <div class="footer-container">
+            
+            <div class="footer-left">
+                <span class="footer-brand">EdTech</span>
+                <div class="icons">
+                    <a href=".">yt</a>
+                    <a href=".">vk</a>
+                    <a href=".">tg</a>
+                    <a href=".">wa</a>
+                </div>
+            </div>
+
+           
+            <div class="footer-middle">
+                <a href="" class="footer-btn">Каталог курсов</a>
+                <a href="" class="footer-btn">Учиться бесплатно</a>
+                <a href="" class="footer-btn">Медиа Нетологии</a>
+            </div>
+
+      
+            <div class="footer-right">
+                <div class="swift-logo">
+                  
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
+
