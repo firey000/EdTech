49cdeba97bb65396aff73dd782a8a6d5610866ec
Kirill9863 <wwwkirill.gonch@gmail.com>
2026-04-28 13:49:57 +0300

    qwe

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="site_footer">
+  <div class="footer_left">
+    <div class="logo">EdTech</div>
+    <div class="social_icons">
+      <div class="icon_box"></div>
+      <div class="icon_box"></div>
+      <div class="icon_box"></div>
+      <div class="icon_box"></div>
+    </div>
+  </div>
+
+  <div class="footer_nav">
+    <a href="#" class="nav_btn">Каталог курсов</a>
+    <a href="#" class="nav_btn">Учиться бесплатно</a>
+    <a href="#" class="nav_btn">Медиа Нетологии</a>
+  </div>
+
+  <div class="footer_right">
+    <div class="placeholder_square"></div>
+  </div>
+</footer>
