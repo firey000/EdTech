a649936f153967191d16ba7791c65fd32be31c88
firey000 <danilmikhailov2008@gmail.com>
2026-04-28 17:31:14 +0300

    страница курса

---
+++ course.php
@@ -0,0 +1,18 @@
+<?php include "header.php"?>
+<main>
+    <div class="section1">
+        <a class="backBtn">← Вернуться</a>
+        <h1>Python с нуля</h1>
+        <div class="section11">
+        <div class="section1Text">
+            <label>Научитесь писать чистый код, автоматизировать задачи и собрать портфолио за 12 недель</label>
+        </div>
+        <div>
+            <img src="./image/python.jpeg" alt="">
+        </div>
+        </div>
+    </div>
+</main>
+<?php include "footer.php"?>
+</body>
+</html>
